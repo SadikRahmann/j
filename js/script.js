1a2895b86c333d250747286328c9f9b9b49c73ba
SadikRahmann <96946882+SadikRahmann@users.noreply.github.com>
2022-04-16 14:43:53 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+
+$("#draggable").draggable();
+
+
+ $( "#draggable2" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "Dropped!" );
+      }
+    });
+
+$( "#sortable" ).sortable();
+
+ $( "#accordion" ).accordion({
+      collapsible: true
+    });
+
+
+
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+    $( "#tags" ).autocomplete({
+      source: availableTags
+    });
+
+    $( "#menu" ).menu();
+
+});
+
